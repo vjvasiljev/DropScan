6587226a8ca6dfab602de0f7c0ddd1f1bcde2d6c
Viaceslav <v.j.vasiljev@gmail.com>
2024-04-27 11:48:49 +0300

    Addet basic wallet connect connectivity

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,54 @@ import LinearProgress from "@mui/joy/LinearProgress";
 
 
 
+import { createWeb3Modal } from '@web3modal/wagmi/react'
+import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
+
+import { WagmiProvider } from 'wagmi'
+import { arbitrum, mainnet } from 'wagmi/chains'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+// 0. Setup queryClient
+const queryClient = new QueryClient()
+
+// 1. Get projectId at https://cloud.walletconnect.com
+//TODO: Hide this in env var
+const projectId = process.env.REACT_APP_WALLET_CONNECT_CLOUD_API;
+
+// 2. Create wagmiConfig
+const metadata = {
+  name: 'Web3Modal',
+  description: 'Web3Modal Example',
+  url: 'https://web3modal.com', // origin must match your domain & subdomain
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
+}
+
+const chains = [mainnet, arbitrum] as const
+const config = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata,
+  // ...wagmiOptions // Optional - Override createConfig parameters
+})
+
+// 3. Create modal
+createWeb3Modal({
+  wagmiConfig: config,
+  projectId,
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+  enableOnramp: true // Optional - false as default
+})
+
+export function Web3ModalProvider({ children }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  )
+}
+
+
+
 
 
 export default function JoyOrderDashboardTemplate() {
@@ -99,6 +147,8 @@ export default function JoyOrderDashboardTemplate() {
             >
               Connect Wallet
             </Button>
+      <w3m-button />
+            
           </Box>
           <Typography level="h3">Adress: 0x... </Typography>
           <Typography level="h3">Total score: .... </Typography>
